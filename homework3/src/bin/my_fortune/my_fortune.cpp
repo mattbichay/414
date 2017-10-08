5d58ceda12c68c576c1e41d8da424371baac3e53
= <mattbichay@gmail.com>
2017-10-07 23:54:59 -0400

    added signals and added comments

--- homework3/src/bin/my_fortune/my_fortune.cpp
+++ homework3/src/bin/my_fortune/my_fortune.cpp
@@ -1,3 +1,8 @@
+/* Name: Matthew Bichay
+ * Course: Systems Development in the Unix Environment
+ * Semester: Fall 2017
+ * Description: Main file for the my_fortune binary.
+ */
 #include <iostream>
 #include <unistd.h>
 #include <limits.h>
@@ -7,14 +12,23 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include "log_mgr.h"
 
 #define QUITCHAR 'q'
 
 using namespace std;
 
+/* Paths to the fortune application */
 const std::string ABSAROKA_FORTUNE = "/home/unix_dev/NOTES/fortune_absaroka/fortune";
 const std::string DEV3_FORTUNE = "/home/unix_dev/NOTES/fortune/fortune";
 
+int origflags;
+int newflags;
+
+/* This method takes a string and returns a copy of that string with all
+ * characters propperly capitalized
+ */
 string upper(string in_str)
 {
     for (int i = 0; i < in_str.size(); ++i)
@@ -22,15 +36,32 @@ string upper(string in_str)
     return in_str;
 }
 
+void revert_flags(int sig)
+{
+    if (fcntl(0, F_SETFL, origflags < 0))
+    {
+        log_event(FATAL, "Error:%s", "original flags unable to be restored");
+        perror("fcntl");
+        exit(1);
+    }
+    exit(sig);
+}
+
 int main()
 {
     char c = '\0';
-    int readrtn, return_val, origflags, newflags;
+
+    /* Make standard input nonblocking */
+    int readrtn, return_val;
     if ((origflags = fcntl(0, F_GETFL, 0)) < 0)
+    {   log_event(FATAL, "Error:%s", "original flags were unable to be attained");
         perror("fcntl F_GETFL error");
+        exit(1);
+    }
     newflags = origflags|O_NONBLOCK;
     if ((return_val = fcntl(0, F_SETFL, newflags)) < 0)
     {
+        log_event(FATAL, "Error:%s", "new flags were unable to be set");
         perror("fcntl");
         exit(1);
     }
@@ -42,24 +73,39 @@ int main()
     string fortune_executable = ABSAROKA_FORTUNE;
     if (string(hostname) == "dev3")
         fortune_executable = DEV3_FORTUNE;
+    log_event(INFO, "hostname:%s", string(hostname));
+    log_event(INFO, "application:%s", string(fortune_executable));
+
+    /* Initialize sufficiently large buffer for pipe */
     char readbuf[2000];
     FILE * pipe_in;
 
+    /* Setup signal handlers */
+    signal(SIGINT, revert_flags);
+
     while(c != QUITCHAR)
     {
+        /* read stdin and write the first character to c */
         if ((readrtn = read(0, &c, 1)) < 0)
         {
+            /* Open pipe */
             if (!(pipe_in = popen(fortune_executable.c_str(), "r")))
             {
+                log_event(FATAL, "Error:%s", "failed to run executable");
                 break;
-            } 
+            }
+            /* Read buffer from opened pipe */
             while (fgets(readbuf, 2000, pipe_in) != NULL)
+                /* Write to stdout using upper method */
                 cout << upper(string(readbuf)) << endl;
             pclose(pipe_in);
+            log_event(INFO, "%s", "fortune propperly printed");
+            
+            /* Random sleep */
             sleep((rand() % 15));
         }
     }
-    if (fcntl(0, F_SETFL, origflags < 0))
-            perror("fcntl");
+    /* Change back to original flags */
+    revert_flags(0);
     return 0;
 }
